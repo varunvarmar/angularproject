d175f0c4f65fe854c6e7103a643274bdd87b6b1b
varunvarmar <varun.v@pacewisdom.com>
2020-12-28 11:22:44 +0530

    Errors Free

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
